bfe4d5c293d34c686257678a03be95b104cba228
AnirudhLab <anirudhpatilmail@gmail.com>
2025-06-08 18:56:51 +0530

    Delete Dental_Clinic_Appointment_RAG_backup directory

--- Dental_Clinic_Appointment_RAG_backup/ingestion/patient_db_loader.py
+++
@@ -1,4 +0,0 @@
-import mysql.connector
-
-def connect_patient_db(config):
-    return mysql.connector.connect(**config)
--- Dental_Clinic_Appointment_RAG_backup/rag/embeddings.py
+++
@@ -1,4 +0,0 @@
-from langchain.embeddings.openai import OpenAIEmbeddings
-
-def get_embeddings(api_key:str):
-    return OpenAIEmbeddings(openai_api_key=api_key)
